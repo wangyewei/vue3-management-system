6e700d007f5f5412ac205f9b941f24118075e05e
WangYeWei <826036140@qq.com>
2022-03-09 19:57:59 +0800

    Update BASE_URL.ts

--- src/service/BASE_URL.ts
+++ src/service/BASE_URL.ts
@@ -4,11 +4,13 @@
  * @WeChat: wj826036
  * @Motto: 求知若渴，虚心若愚
  * @Description: base api url
- * @LastEditTime: 2022-03-09 19:38:55
+ * @LastEditTime: 2022-03-09 19:57:41
  * @Version: 1.0
  * @FilePath: \vue3-management-system\src\service\BASE_URL.ts
  */
 
-const BASED_APIURL = process.env.NODE_ENV === 'development' ? 'http://10.40.34.214:3005' : 'https://api.06k4.com'
+// const BASED_APIURL = process.env.NODE_ENV === 'development' ? 'http://10.40.34.214:3005' : 'https://api.06k4.com'
+
+const BASED_APIURL = 'https://api.06k4.com'
 
 export { BASED_APIURL }
